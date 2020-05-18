33d0e73e258422c04f05029a15cbb87ad6841f67
Yang <kshjam63@naver.com>
2020-05-18 22:46:49 +0900

    fix

--- src/stories/BooleanComponentStory.jsx
+++ src/stories/BooleanComponentStory.jsx
@@ -2,3 +2,7 @@ import React from 'react';
 import { storiesOf } from "@storybook/react";
 
 import BooleanComponent from '../03/BooleanComponent';
+
+storiesOf('BooleanComponent', module)
+    .add('Default', () => <BooleanComponent />)
+    .add('bored', () => <BooleanComponent bored />)
